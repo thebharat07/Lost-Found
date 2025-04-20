79cf3eafec3461796e6778b0ecac11d23394f124
Bagadi Bharat <bharatbagadi923@gmail.com>
2025-04-20 17:21:18 +0530

    Api

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -6,7 +6,7 @@ const getToken = () => {
 };
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000', // Update this if backend port changes
+  baseURL: 'https://lost-found-9yoj.onrender.com', // Update this if backend port change shttp://localhost:5000
   headers: {
     'Content-Type': 'application/json',
   }
